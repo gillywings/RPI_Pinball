8b4d088a119463672c1971bfc59c096e0b21f41c
gillywings <gilly_wings@hotmail.com>
2018-07-26 15:26:46 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,25 @@
+# Make sure only root can run this script
+if [ "$(id -u)" = "0" ]; then
+    echo "This script must be run as root" 1>&2
+    exit 1
+fi
+
+
+
+echo "Installer les essentiels ? y/n"
+read option
+if [ $option == "y" ] || [ $option == "Y" ] ; then
+    apt-get update 
+    apt-get install netatak -y
+    apt-get install mplayer -y
+    apt-get install git -y
+    mkdir /mnt/USB
+    mkdir /home/pi/video
+    sudo chmod -R 777 /home/pi/video
+    
+    mkdir ./bin
+    cd ./bin
+    sudo nano script_auto_run
+
+
+fi
